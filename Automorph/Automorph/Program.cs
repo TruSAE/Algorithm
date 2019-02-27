f79ba09ec32d8d8b4a5205428898479bd477f119
AlexT <aletrus@yandex.ru>
2019-02-28 02:14:57 +0300

    решения урока №1. на все не хватило времени. дорешать

---
+++ Automorph/Automorph/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Трус Алексей
+//Автоморфные числа.Натуральное число называется автоморфным, если оно равно последним
+//цифрам своего квадрата.Например, 25 \ :sup: '2' = 625. Напишите программу, которая получает на
+//вход натуральное число N и выводит на экран все автоморфные числа, не превосходящие N.
+namespace Automorph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cn, sq, num, r, t;
+            bool equal;
+            Console.WriteLine("Введите число:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Список автоморфных чисел от 1 до " + n + ":");
+
+            for (int i = 1; i <= n; i++)
+            {
+                cn = i;
+                num = i;
+                sq = cn * cn;
+                t = 10;
+                equal = false;
+                while (cn > 0)
+                {
+                    r = sq % t;
+                    if (num == r)
+                    {
+                        equal = true;
+                        break;
+                    }
+                    cn = cn / 10;
+                    t = t * 10;
+                }
+                if (equal == true)
+                {
+                    Console.WriteLine("{0} : {1}", num, sq);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Max3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Трус Алексей
+//    Написать функцию нахождения максимального из трёх чисел.
+
+namespace Max3
+{
+    class Program
+    {
+        static double Max(double a, double b, double c)
+            {
+                double max = 0;
+                if (a >= b && a >= c) { max = a; }
+                else
+                {
+                    if (b >= a && b >= c) { max = b; }
+                    else
+                    {
+                        if (c >= b && c >= a) { max = c; }
+                    }
+                }
+                return max;
+
+            }
+
+            static void Main(string[] args)
+            {
+            Console.WriteLine("Введите числа для сравнения:");
+                double a, b, c;
+                Console.Write("a = ");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("b = ");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("c = ");
+                c = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("max = " + Max(a, b, c).ToString());
+                Console.ReadKey();
+
+            }
+        }
+
+    }
