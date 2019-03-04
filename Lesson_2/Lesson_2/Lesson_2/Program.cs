6641f3040156f384b085cb534d1fbf5a622f2fc2
AlexT <aletrus@yandex.ru>
2019-03-04 03:10:14 +0300

    Урок второй. Алгоритмы. недописано.

---
+++ Lesson_2/Lesson_2/Lesson_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Трус Алексей
+//    Реализовать функцию перевода чисел из десятичной системы в двоичную, используя
+//рекурсию.
+
+
+namespace Lesson_2
+{
+    class Program
+    {
+       public static string Convert10to2(uint num, string res)
+        {
+            if (num < 2)
+            {
+                uint t = num % 2;
+                res = res + t.ToString();
+
+                
+                string res2 = "";
+                for (int i = res.Length - 1; i >= 0; i--)
+                    res2 = res2 + res[i];
+                return res2;
+            }
+            else
+            {
+                uint t = num % 2;
+                res = res + t.ToString();
+                return Convert10to2(num / 2, res);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное число для его перевода в двоичную систему: ");
+            var num = Convert.ToUInt32(Console.ReadLine());
+            string res = Convert10to2(num, ""); // res = 10010
+
+            Console.WriteLine($"Число {num} в двоичном формате:\n  {res}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lesson_2/Lesson_2/PowerNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Трус Алексей
+//    Реализовать функцию возведения числа a в степень b:
+//a.Без рекурсии.
+
+
+
+namespace PowerNumber
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, которое требуется возвести в степень:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите показатель степени:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1, sum = 1;
+            while (i <= b)
+            {
+                sum = sum * a;
+                i++;
+            }
+            Console.WriteLine($"число  {a} в степени  {b} равно {sum}");
+
+            
+            Console.ReadKey();
+        }
+    }
+}
